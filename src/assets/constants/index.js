bd5c88dbcab8813b088dd055e79ffcfecdfa5748
ProgrammerVijender <Vijenderkumar4pc@gmail.com>
2025-08-21 10:49:54 +0530

    updated

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,32 +1,24 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+import project1 from "../assets/projects/Jobportal.png";
+import project2 from "../assets/projects/Ayurveda.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer and currently i am in a final year of B.Tech in Information Technology from GGSIPU university. I have 1 year of hands-on experience, I have learned skills in front-end technologies like React and Tailwind, as well as back-end technologies like Node.js and MySQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer and currently i am in a final year of B.Tech in Information Technology from GGSIPU university. I have 1 year of hands-on experience, I have learned skills in front-end technologies like React and Tailwind, as well as back-end technologies like Node.js, MySQL, Mongodb. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Tailwind, Node.js, MySQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
 
 export const PROJECTS = [
   {
-    title: "FastFood Webpage",
+    title: "Job Portal",
     image: project1,
     description:
-      "A fastfood website landing page with enhanced colours combiniation.",
-    technologies: ["HTML", "CSS"],
+      "A full stack job portal application that connects job seekers with employers, allowing users to post jobs, apply for positions, and manage their profiles.",
+    technologies: ["Reactjs", "Nodejs", "Mongodb"," Tailwind CSS", "Express.js"],
   },
   {
-    title: "Rock Paper Scissors",
+    title: "Ayurveda Ai Plant identification System",
     image: project2,
     description:
-      "A javascript project which helps me to learn and enhance my javascript skills.",
-    technologies: ["HTML", "CSS", "Javascript"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "Javascript"],
+      "A mern stack application for identifying plants using AI and share thier health benfits with the Ayurveda Community.",
+    technologies: ["Reactjs", "Nodejs", "Mongodb"," Tailwind CSS", "Express.js","cloudinary","Google AI API"],
   },
 ];
 
